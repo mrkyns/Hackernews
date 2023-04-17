57203a07b545c390fa28ee6d5358b3a76894f358
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-04-17 16:08:54 +0200

    final initial structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
