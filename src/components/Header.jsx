8b9fbfe3d9b074f3620fdca6c0f93588a1d5f2f1
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-04-17 20:32:00 +0200

    Header structure and added CSS structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,52 @@
-export default function Header() {}
+export default function Header() {
+  return (
+    <div className="Header">
+      <div className="navigation">
+        <svg
+          version="1.1"
+          id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
+          xlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 213.9 213.9"
+          space="preserve"
+        >
+          <g>
+            <path d="M0,0v213.9h213.9V0H0z M208.8,208.8H5.1V5.1h203.6V208.8z" />
+            <polygon points="99.1,187.6 115.3,187.6 115.3,124.8 166.9,26.3 149,26.3 107.2,110.1 65.2,26.3 47,26.3 99.1,124.8 	" />
+          </g>
+        </svg>
+        <nav>
+          <span>Hacker News</span>
+          <ul>
+            <li>
+              <a href="#">new</a>
+            </li>
+            <li>
+              <a href="#">past</a>
+            </li>
+            <li>
+              <a href="#">comments</a>
+            </li>
+            <li>
+              <a href="#">ask</a>
+            </li>
+            <li>
+              <a href="#">show</a>
+            </li>
+            <li>
+              <a href="#">jobs</a>
+            </li>
+            <li>
+              <a href="#">submit</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="login">
+        <a href="#">Login</a>
+      </div>
+    </div>
+  );
+}
