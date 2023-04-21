880b40fd5a1aeabbab6046433f252bc1784b470e
Adam <adamna1989@gmail.com>
2023-04-21 11:56:11 +0100

    added search on submit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -12,16 +12,21 @@ export default function Search( { setQuery } ) {
     setQuery(inputText)
   };
 
+let submitHandler = (e) => {
+  e.preventDefault();
+  setQuery(inputText)
+}
   
 
   return (
     <div className="Search">
       {/* <div className="search_wrapper"> */}
-        <form className="search_wrapper" onChange={inputHandler}>
+        <form className="search_wrapper" onSubmit={submitHandler}>
           <input
             className="search_input"
             type="search"
             placeholder="Enter your query"
+            onChange={inputHandler}
           />
           <button className="search_button">Search</button>
         </form>
