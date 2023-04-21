cd24aac5b195c2dec13ebad0fff5db4004b8ef57
Miroslav Ljubicic <127101426+mrkyns@users.noreply.github.com>
2023-04-21 10:01:41 +0200

    automatic Reagan mode

--- src/components/Reagan.jsx
+++ src/components/Reagan.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Reagan() {
   const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      document.body.classList.add("reagan_mod");
+      setActive(!active);
+    }
+  }, []);
+
   return (
     <div
       onClick={function toggleBtn() {
